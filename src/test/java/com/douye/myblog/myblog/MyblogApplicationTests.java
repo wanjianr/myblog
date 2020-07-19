2a5e5130aff2928177d031d9085cd835ffb7b9eb
wanjianr <1041949906@qq.com>
2020-07-19 19:08:06 +0800

    login test

---
+++ src/test/java/com/douye/myblog/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douye.myblog.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
