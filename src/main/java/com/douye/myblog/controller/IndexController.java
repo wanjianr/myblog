712fb40e3986dc2da9b85388750f769b282639c6
douye <1041949906@qq.com>
2020-07-22 19:37:33 +0800

    publish question

--- src/main/java/com/douye/myblog/controller/IndexController.java
+++ src/main/java/com/douye/myblog/controller/IndexController.java
@@ -20,16 +20,17 @@ public class IndexController {
     @GetMapping("/")
     public String index(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                String value = cookie.getValue();
-                User user = userMapper.findByToken(value);
-                if (user != null) {
-                    request.getSession().setAttribute("user",user);
+        if (cookies != null)
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String value = cookie.getValue();
+                    User user = userMapper.findByToken(value);
+                    if (user != null) {
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
                 }
-                break;
             }
-        }
         return "index";
     }
 
---
+++ src/main/java/com/douye/myblog/mapper/QuestionMapper.java
@@ -0,0 +1,15 @@
+package com.douye.myblog.mapper;
+
+import com.douye.myblog.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,comment_count,view_count,like_count,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{commentCount},#{viewCount},#{likeCount},#{tag})")
+    void create(Question question);
+
+}
